ed959a8abca968f24478ad6ca70d0433a01e9aff
shaloo <5890484+shaloo@users.noreply.github.com>
2023-10-10 10:51:12 +0530

    Upgrade Auth v1.0.8

--- connectors/ArcanaWallet.ts
+++ connectors/ArcanaWallet.ts
@@ -4,9 +4,10 @@ import { AuthProvider } from "@arcana/auth";
 import { URLS } from "../chains";
 
 const auth = new AuthProvider(
-  "xar_test_d24f70cd300823953dfa2a7f5b7c7c113356b1ad",
+  //"xar_test_d24f70cd300823953dfa2a7f5b7c7c113356b1ad",
+  "xar_live_d7c88d9b033d100e4200d21a5c4897b896e60063",
   {
-    theme: "light",
+    theme: "dark",
     connectOptions: {
       compact: true
     }
