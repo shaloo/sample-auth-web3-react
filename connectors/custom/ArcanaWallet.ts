d26cfaf5c1f5fd5c1bc4302de2b9949e5b369863
shaloo <5890484+shaloo@users.noreply.github.com>
2023-11-06 13:36:30 +0530

    Refs: #AR-7087 fixes for custom login UI

---
+++ connectors/custom/ArcanaWallet.ts
@@ -0,0 +1,24 @@
+import { initializeConnector } from "@web3-react/core";
+import { ArcanaConnector } from "@arcana/auth-web3-react";
+import { AuthProvider } from "@arcana/auth";
+import { URLS } from "../chains";
+
+const auth = new AuthProvider(
+  //"xar_test_d24f70cd300823953dfa2a7f5b7c7c113356b1ad",
+  "xar_live_d7c88d9b033d100e4200d21a5c4897b896e60063",
+  {
+    theme: "dark",
+    connectOptions: {
+      compact: true
+    }
+  }
+);
+export const [arcanaConnect, hooks] = initializeConnector<ArcanaConnector>(
+  (actions) =>
+    new ArcanaConnector(auth, {
+      actions,
+      login: {
+        provider: 'google',
+      } // either add here or in setLogin function
+    })
+);
